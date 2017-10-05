88fab43b69808aa247f3be00b2afab8d4e534375
rajesh23-drg <rmanjunath@teamdrg.com>
2017-10-05 12:42:03 +0530

    w.i.p

--- node-auth-passport/routes/users.js
+++ node-auth-passport/routes/users.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var multer= require('multer');
 var upload = multer({dest: './uploads'});
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -49,6 +51,22 @@ router.post('/register', upload.single('profileimage'), function(req, res, next)
     res.render('register', {errors: errors});
   }else{
     console.log('noerrors')
+    var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileimage: profileimage
+    });
+
+    User.createUser(newUser,function(err, user){
+      if(err) throw err;
+      console.log(user);
+    });
+
+    req.flash('success', 'you are now registered and can login!');
+    res.location('/');
+    res.redirect('/');
   }
 });
 
