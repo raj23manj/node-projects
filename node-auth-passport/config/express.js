801dadb09b9d5112a24dd9f27a151eeb653188ec
rajesh23-drg <rmanjunath@teamdrg.com>
2017-10-07 20:34:12 +0530

    refractored

--- node-auth-passport/config/express.js
+++ node-auth-passport/config/express.js
@@ -17,65 +17,64 @@ var expressValidator = require('express-validator');
 var bcrypt = require('bcryptjs');
 
 module.exports = function(app, currentDirectory) {
-  // view engine setup
-app.set('views', path.join(currentDirectory, 'views'));
-app.set('view engine', 'jade');
+    // view engine setup
+  app.set('views', path.join(currentDirectory, 'views'));
+  app.set('view engine', 'jade');
 
 
-//Handle Sessions
-app.use(session({
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: true
-}));
+  //Handle Sessions
+  app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+  }));
 
-// Passport
-app.use(passport.initialize());
-app.use(passport.session());
+  // Passport
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-// validator
-app.use(expressValidator({}));
+  // validator
+  app.use(expressValidator({}));
 
-// express-messages/connect-flash
-app.use(require('connect-flash')());
-app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
-  next();
-});
+  // express-messages/connect-flash
+  app.use(require('connect-flash')());
+  app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+  });
 
-// to set global loggedin user
-app.get('*', function(req, res, next){
-  res.locals.user = req.user || null;
-  next();
-});
+  // to set global loggedin user
+  app.get('*', function(req, res, next){
+    res.locals.user = req.user || null;
+    next();
+  });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(currentDirectory, 'public')));
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(currentDirectory, 'public')));
 
-//app.use('/', index);
-require('../config/routes')(app);
+  // require all the routes
+  require('../config/routes')(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-}
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+};
