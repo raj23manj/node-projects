7bc24dd7a3811af33702238bd229b1fd96f9e0ee
rajesh23-drg <rmanjunath@teamdrg.com>
2017-10-09 11:49:40 +0530

    tests are working with cheerio

--- node-auth-passport/test/users-spec.js
+++ node-auth-passport/test/users-spec.js
@@ -1,15 +1,29 @@
 const request = require('supertest');
 const expect = require('chai').expect;
 const app = require('../app');
-//const agent = request.agent(app);
 const cheerio = require("cheerio");
 
 describe("Users Routes", () => {
 
+  it("should Load User's Root Page and have a response", (done) => {
+    request(app).get('/users/').expect(200).end((err, res) =>{
+      expect(res.text).to.equal("respond with a resource");
+      done();
+    });
+  });
+
   it("Should Load the User Register Page", (done) => {
       request(app).get("/users/register").end((err, res) =>{
-          console.log(res.body);
           expect(res.status).to.equal(200);
+          done();
+      });
+  });
+
+  it("should have header as register", (done) => {
+    request(app).get("/users/register").end((err, res) =>{
+          let $ = cheerio.load(res.text);
+          let pageHeading = $("body div.container h2.page-header").text();
+          expect(pageHeading).to.equal("Register");
           done();
       });
   });
