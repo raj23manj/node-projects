2c03ac047df1689f503f986dc0932c9a0ff3a2ed
rajesh23-drg <rmanjunath@teamdrg.com>
2017-10-10 11:10:53 +0530

    post tests

--- node-auth-passport/test/users-spec.js
+++ node-auth-passport/test/users-spec.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const expect = require('chai').expect;
 const app = require('../app');
 const cheerio = require("cheerio");
+//const User = require('../models/user');
 
 describe("Users Routes", () => {
 
@@ -28,10 +29,68 @@ describe("Users Routes", () => {
       });
   });
 
-  it("should post a register", (done) => {
-    request(app).post("/users/register").end((err, res) => {
-      
-    });
+// wrong implementation it will be success only
+  // it("should post a register", (done) => {
+  //   request(app).post("/users/register")
+  //               .field('username', 'a')
+  //               .field('password', '1234')
+  //               .expect(200)
+  //               .end((err, res) => {
+  //                 done();
+  //   });
+  // });
+
+  it("should  login", (done) => {
+    request(app).post("/users/login")
+                .send({
+                        username: 'a',
+                        password: '1234'
+                      })
+                .expect('Location', '/')
+                .end(done);
+  });
+
+  it("should  login23", (done) => {
+    request(app).post("/users/login")
+                .send({
+                        username: 'a',
+                        password: '1234'
+                      })
+                .end((err, res) => {
+                  console.log('@@@@@@@@@@@@@@@@');
+                  console.log(res.text);
+                  done();
+                });
   });
 
 });
+
+
+// https://stackoverflow.com/questions/20164478/unit-test-login-with-passport-js-and-express-js
+// describe('User', function () {
+//   before(function(done) {
+//       user = new User({
+//         email    : "user@user.com",
+//         firstName: "Full Name",
+//         lastName : "Last Name",
+//         password : "pass11"
+//       });
+//       user.save(done)
+//     });
+//   describe('Login test', function () {
+//       it('should redirect to /', function (done) {
+//         agent
+//         .post('/users/session')
+//         .field('email', 'user@user.com')
+//         .field('password', 'pass11')
+//         .expect('Location','/')
+//         .end(done)
+//       })
+
+//   after(function(done) {
+//       User.remove().exec();
+//       return done();
+//     });
+
+// })
+// })
