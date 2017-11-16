7a74b64656ff3f7d57163b09df2a9e9e8c7ef0e9
rajesh23-drg <rmanjunath@teamdrg.com>
2017-11-16 12:37:22 +0530

    Added Testing

---
+++ node-blog/test/categories-spec.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const expect = require('chai').expect;
+const app = require('../app');
+const cheerio = require("cheerio");
+
+
+describe("Categories Routes", () => {
+  it("should Get '/categories/show/:category'", (done) => {
+    request(app).get('/categories/show/Technology').expect(200).end((err, res) =>{
+      let $ = cheerio.load(res.text);
+      let divs = $("body").find('div.post').length;
+      expect(divs).to.equal(2);
+      done();
+    });
+  });
+});
